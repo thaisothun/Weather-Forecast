84f9cc8cbf609cbc118afb62bcb36c06e094a2b8
thaisothun <thaisothun@gmail.com>
2026-01-17 15:23:58 +0700

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -48,7 +48,7 @@ def get_weather(city):
 
 def current_city(request):
     ip_address = requests.get('https://api.iplocate.io/json').json()
-    location_url = f'https://iplocate.io/api/lookup/{ip_address['ip']}?apikey=444e70c687d6334254a9a997e1ccacab'
+    location_url = f"https://iplocate.io/api/lookup/{ip_address['ip']}?apikey=444e70c687d6334254a9a997e1ccacab"
     location = requests.get(location_url).json()
     current_city = location['city']
     city1 = request.POST.get('city1','NA')
