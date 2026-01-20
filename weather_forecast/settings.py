59a75200f66cc17b35ba5d58844e24e784fc64ea
thaisothun <thaisothun@gmail.com>
2026-01-20 16:38:21 +0700

    Update settings.py

--- weather_forecast/settings.py
+++ weather_forecast/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles/static"/
+STATIC_ROOT = BASE_DIR / 'staticfiles/static'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'static')
 ]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
