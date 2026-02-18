eb7e6a44eb92ad46940c05f18cecbf7f0042a23c
thaisothun <thaisothun@gmail.com>
2026-02-18 21:40:51 +0700

    Update settings.py

--- weather_forecast/settings.py
+++ weather_forecast/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-4i_md_7t&*#2y(_5c@2b*-+(tl)9qk9cich*li9y1niku9=1%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.cloudfront.net',
+]
+
+ALLOWED_HOSTS = ['*','d3ntfexyy3qov9.cloudfront.net']
 
 
 # Application definition
